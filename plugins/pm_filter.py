c1f4bd131819a6ebd3c3a19190cdb369d0cfd8c1
Mahi-Sen <155892772+Mahi-Sen@users.noreply.github.com>
2024-12-18 01:21:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2666,7 +2666,6 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
         cap+= "<b><blockquote>🍿 ʏᴏᴜʀ ꜰɪʟᴇꜱ 👇</blockquote></b>\n\n"
         for file in files:
             cap += f"<b>🗄️ <a href='https://telegram.me/{temp.U_NAME}?start=files_{file['file_id']}'>[{get_size(file['file_size'])}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file['file_name'].split()))}</a></b>\n\n"
-	cap+= "<b><blockquote>🍿 ʏᴏᴜʀ ꜰɪʟᴇꜱ 👇</blockquote></b>"
 	
     if imdb and imdb.get('poster'):
         try:
